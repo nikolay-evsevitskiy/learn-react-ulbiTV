5332fbc01e10f9d9f8218cfc7bd80d147b6843dd
Nikolay Evsevitski <nikolay.evsevitskiy@gmail.com>
2023-07-11 20:52:04 +0200

    learn useState

--- src/App.js
+++ src/App.js
@@ -1,10 +1,22 @@
-import React from 'react';
-function App() {
-  return (
-    <div className="App">
+import React, {useState} from 'react';
 
-    </div>
-  );
+function App() {
+    let [likes, setLikes] = useState(0);
+    const incrementValue = () => {
+        setLikes(likes += 1)
+        return console.log(likes)
+    }
+    const decrementValue = () => {
+        setLikes(likes -= 1)
+        return console.log(likes)
+    }
+    return (
+        <div>
+            <h1>{likes}</h1>
+            <button onClick={incrementValue}> increment </button>
+            <button onClick={decrementValue}> decrement </button>
+        </div>
+    );
 }
 
 export default App;
