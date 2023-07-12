d4fee2a345f1d94c168e98c4bae695f5a4b87926
Nikolay Evsevitski <nikolay.evsevitskiy@gmail.com>
2023-07-12 14:57:55 +0200

    two-way binding

--- src/App.js
+++ src/App.js
@@ -1,20 +1,27 @@
 import React, {useState} from 'react';
 
 function App() {
-    let [likes, setLikes] = useState(0);
+    const [likes, setLikes] = useState(0);
+    const [value, setValue] = useState('');
     const incrementValue = () => {
-        setLikes(likes += 1)
+        setLikes(likes + 1)
         return console.log(likes)
     }
     const decrementValue = () => {
-        setLikes(likes -= 1)
+        setLikes(likes - 1)
         return console.log(likes)
     }
+    const inputHandler = (e) => {
+        setValue(e.currentTarget.value)
+    }
+    
     return (
         <div>
             <h1>{likes}</h1>
-            <button onClick={incrementValue}> increment </button>
-            <button onClick={decrementValue}> decrement </button>
+            <h2>{value}</h2>
+            <input type="text" value={value} onChange={inputHandler}/>
+            <button onClick={incrementValue}> increment</button>
+            <button onClick={decrementValue}> decrement</button>
         </div>
     );
 }
