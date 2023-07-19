6b8defd38229bd829cb8e07fdb0a37754230562a
Nikolay Evsevitski <nikolay.evsevitskiy@gmail.com>
2023-07-19 18:13:30 +0200

    create Loader component

--- src/App.js
+++ src/App.js
@@ -6,12 +6,14 @@ import {PostFilter} from "./components/PostFilter";
 import {MyModal} from "./components/UI/myModal/MyModal";
 import {MyButton} from "./components/UI/button/MyButton";
 import {usePosts} from "./hooks/usePosts";
-import axios from "axios";
+import PostService from "./api/postService";
+import {Loader} from "./components/UI/loader/Loader";
 
 function App() {
     const [posts, setPosts] = useState([])
     const [filter, setFilter] = useState({sort: '', query: ''})
     const [modal, setModal] = useState(false)
+    const [isPostLoader, setSetIsPostLoader] = useState(false)
     const sortedAndSearchedPosts = usePosts(filter.sort, posts, filter.query)
 
     useEffect(() => {
@@ -20,8 +22,10 @@ function App() {
 
 
     async function fetchPosts() {
-        const response = await axios.get('https://jsonplaceholder.typicode.com/posts')
-        setPosts(response.data)
+        setSetIsPostLoader(true)
+        const posts = await PostService.getAll()
+        setPosts(posts)
+        setSetIsPostLoader(false)
     }
 
     const deletePost = (postId) => {
@@ -51,10 +55,15 @@ function App() {
                 filter={filter}
                 setFilter={setFilter}
             />
-            <PostList
-                posts={sortedAndSearchedPosts}
-                deletePost={deletePost}
-            />
+            {
+                isPostLoader
+                    ? <Loader/>
+                    : <PostList
+                        posts={sortedAndSearchedPosts}
+                        deletePost={deletePost}
+                    />
+            }
+
         </div>
     );
 }
---
+++ src/components/UI/loader/Loader.jsx
@@ -0,0 +1,7 @@
+import style from './loader.module.css'
+
+export const Loader = () => {
+    return <div className={style.mainContainer}>
+        <div className={style.loader}/>
+    </div>
+}
