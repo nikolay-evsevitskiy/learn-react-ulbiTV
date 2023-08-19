68f55d1780b02679479710b7aa515149e23a80ba
Nikolay Evsevitski <nikolay.evsevitskiy@gmail.com>
2023-08-19 12:14:46 +0200

    bug fixes in routing

--- src/App.js
+++ src/App.js
@@ -1,37 +1,20 @@
 import React from 'react';
-import style from './style/App.module.css';
-import {
-    createBrowserRouter, Link,
-    RouterProvider,
-} from "react-router-dom";
-import About from "./pages/About";
-import Posts from "./pages/Posts";
+import {Link, Outlet} from "react-router-dom";
+import style from "./style/Posts.module.css";
 
 
 function App() {
-    const router = createBrowserRouter([
-        {
-            path: "/",
-            element: <div>Hello world!</div>,
-        },
-        {
-            path: '/about',
-            element: <About/>,
-        },
-        {
-            path: '/posts',
-            element: <Posts/>,
-        },
-    ]);
     return <>
         <div className={style.navbar}>
             <div className={style.navbarItem}>
-                <a href="/about">About</a>
-                <a href="/posts">Posts</a>
+                <Link to="/about">About</Link>
+                <br/>
+                <Link to="/posts">Posts</Link>
             </div>
         </div>
-        <RouterProvider router={router}/>
+        <Outlet/>
     </>
+
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,12 +1,33 @@
 import React from 'react';
-import {StrictMode} from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App';
+import {createBrowserRouter, RouterProvider} from "react-router-dom";
+import Posts from "./pages/Posts";
+import About from "./pages/About";
+import App from "./App";
+import ErrorPage from "./pages/ErrorPage";
+
+const router = createBrowserRouter([
+    {
+        path: "/",
+        element: <App/>,
+        errorElement: <ErrorPage/>,
+        children: [
+            {
+                path: 'about',
+                element: <About/>,
+            },
+            {
+                path: 'posts',
+                element: <Posts/>,
+            },
+        ]
+    },
+]);
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-    <StrictMode>
-        <App/>
-    </StrictMode>
+    <React.StrictMode>
+        <RouterProvider router={router}/>
+    </React.StrictMode>
 );
 
