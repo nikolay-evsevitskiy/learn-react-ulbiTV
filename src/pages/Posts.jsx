df46fcb25c8bd290326e1bfd1d69d079cc5a60f2
Nikolay Evsevitski <nikolay.evsevitskiy@gmail.com>
2023-09-11 23:00:10 +0200

    create endless scrolling

--- src/pages/Posts.jsx
+++ src/pages/Posts.jsx
@@ -1,4 +1,4 @@
-import React, {useEffect, useState} from 'react';
+import React, {useEffect, useRef, useState} from 'react';
 import style from '../style/Posts.module.css'
 import {usePosts} from "../hooks/usePosts";
 import {getPageCount, getPagesArray} from "../utils/pages";
@@ -11,6 +11,8 @@ import {PostFilter} from "../components/PostFilter";
 import {Loader} from "../components/UI/loader/Loader";
 import PostList from "../components/PostList";
 import Pagination from "../components/UI/pagination/Pagination";
+import {useObserver} from "../hooks/useObserver";
+import {MySelect} from "../components/UI/select/MySelect";
 
 function Posts() {
     const [posts, setPosts] = useState([])
@@ -21,16 +23,21 @@ function Posts() {
     const [page, setPage] = useState(1)
     const sortedAndSearchedPosts = usePosts(filter.sort, posts, filter.query)
     const pagesArray = getPagesArray(totalPages)
+    const lastElement = useRef()
     const [fetchPosts, isPostLoader, postError] = useFetching(async () => {
         const response = await PostService.getAll(limit, page)
         const totalCount = response.headers['x-total-count']
         setTotalPages(getPageCount(totalCount, limit))
-        setPosts(response.data)
+        setPosts([...posts, ...response.data])
+    })
+
+    useObserver(lastElement, page < totalPages, isPostLoader, () => {
+        setPage(page + 1)
     })
 
     useEffect(() => {
-        fetchPosts()
-    }, [page])
+        fetchPosts(limit, page)
+    }, [page, limit])
 
     const deletePost = (postId) => {
         const newPosts = posts.filter(i => i.id !== postId)
@@ -59,23 +66,30 @@ function Posts() {
                 filter={filter}
                 setFilter={setFilter}
             />
+            <MySelect value={limit}
+                      onChange={value => setLimit(value)}
+                      defaultValue={'number of elements per page'}
+                      options={[
+                          {value: 5, name: '5'},
+                          {value: 10, name: '10'},
+                          {value: 25, name: '25'},
+                          {value: -1, name: 'show all'},
+                      ]}
+            />
             {postError &&
                 <h1>
                     {'An error has occurred. ' + postError}
                 </h1>
             }
-            {
-                isPostLoader
-                    ? <Loader/>
-                    : <PostList
-                        posts={sortedAndSearchedPosts}
-                        deletePost={deletePost}
-                    />
-            }
-            <Pagination
-                page={page}
-                setPage={setPage}
-                pagesArray={pagesArray}
+            <PostList posts={sortedAndSearchedPosts}
+                      deletePost={deletePost}
+            />
+            <div ref={lastElement} style={{height: 20, width: '63%'}}/>
+            {isPostLoader && <Loader/>}
+
+            <Pagination page={page}
+                        setPage={setPage}
+                        pagesArray={pagesArray}
             />
 
         </div>
